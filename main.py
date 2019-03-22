227af92cf97f8a6731561cbd935e226d2e318137
Gianluigi Dal Toso <gianluigi.toso@gmail.com>
2019-03-22 04:31:58 -0300

    First Running Maze

--- main.py
+++ main.py
@@ -8,12 +8,16 @@ import pygame
 import os
 import sys
 from pygame.locals import *
+from random import randint
+
+# Import local settings
+from config import *
 
 
 # Initialize game display
 def initalize_display(argv):
     pygame.init()
-    screen = pygame.display.set_mode((1000, 700))
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_LENGTH))
     pygame.display.set_caption('Rainbow Trap')
 
     return screen
@@ -21,20 +25,52 @@ def initalize_display(argv):
 
 # Class for Maze generation
 class Maze:
-    def __init__():
-        print('')
+    def __init__(self, x_length, y_length):
+        print('WIP')
 
 
 # Class for Temporary grid used to generate maze before showing in the screen
 class TempBlock:
-    def __init__():
-        print('')
+    def new_grid(self):
+        grid = []
+        for i in range(self.y_length):
+            grid_line = []
+            for j in range(self.x_length):
+                cell = MazeCell()
+                if randint(0, 9) % 2 == 1:
+                    cell.cell_state = INVALID
+                grid_line.append(cell)
+            grid.append(grid_line)
+        return grid
+
+    def __init__(self, cell_size, x_length, y_length):
+        self.cell_size = cell_size
+        self.x_length = x_length
+        self.y_length = y_length
+        self.grid = self.new_grid()
+
+    def get_line(self):
+        if self.grid:
+            return self.grid.pop()
+        else:
+            self.grid = self.new_grid()
+            return self.grid.pop()
+
+    def get_grid(self):
+        if self.grid:
+            return self.grid
+        else:
+            self.grid = self.new_grid()
+            return self.grid
+
+    def renew_grid(self):
+        self.grid = self.new_grid()
 
 
 # Class for each maze cell used to generate the maze
 class MazeCell:
-    def __init__():
-        print('')
+    def __init__(self):
+        self.cell_state = AVAIABLE
 
 
 # Class for pool of possible objects to be inserted in the maze
@@ -49,7 +85,7 @@ class Kiko:
         self.color = color
         self.size = size
         self.skin = pygame.Surface((size, size))
-        self.pos = (495, 50)
+        self.pos = (INITIAL_X, INITIAL_Y)
         self.move = STAY
         self.skin.fill(self.color)
 
@@ -58,11 +94,11 @@ class Kiko:
 
     def moving(self):
         if self.move == RIGHT:
-            if (self.pos[0]+2*self.size <= 1000):
-                self.pos = (self.pos[0]+self.size, 50)
+            if (self.pos[0]+2*self.size <= SCREEN_WIDTH):
+                self.pos = (self.pos[0]+self.size, INITIAL_Y)
         if self.move == LEFT:
             if (self.pos[0]-self.size >= 0):
-                self.pos = (self.pos[0]-self.size, 50)
+                self.pos = (self.pos[0]-self.size, INITIAL_Y)
 
     def choose_color(self):
         if pygame.key.get_pressed()[K_a]:
@@ -70,7 +106,7 @@ class Kiko:
                 self.change_color(VIOLET)
             elif pygame.key.get_pressed()[K_s]:
                 self.change_color(INDIGO)
-            else: 
+            else:
                 self.change_color(BLUE)
         elif pygame.key.get_pressed()[K_d]:
             if pygame.key.get_pressed()[K_w]:
@@ -81,7 +117,7 @@ class Kiko:
                 self.change_color(RED)
         elif pygame.key.get_pressed()[K_w]:
             self.change_color(YELLOW)
-        else: 
+        else:
             self.change_color(GREEN)
 
     def change_color(self, color):
@@ -92,12 +128,17 @@ class Kiko:
 # Main game flow
 def main(argv):
     screen = initalize_display(argv)
-
+    time = 0
     clock = pygame.time.Clock()
-    kiko = Kiko(10, RED)
+    kiko = Kiko(PLAYER_SIZE, RED)
+    grid_x = SCREEN_WIDTH // FAT_X
+    grid_y = SCREEN_LENGTH // FAT_Y
+    maze = TempBlock(SCREEN_LENGTH // grid_x, grid_x, grid_y)
+    grid = maze.get_grid()
+    maze.renew_grid()
 
     while True:
-        clock.tick(50)
+        time = time + clock.tick(50)
         # If user closes the window, quit the game
         for event in pygame.event.get():
             if event.type == QUIT:
@@ -125,6 +166,22 @@ def main(argv):
 
         screen.fill((0, 0, 0))
         screen.blit(kiko.skin, kiko.pos)
+        if not grid:
+            grid = maze.get_grid()
+        count_y = 200
+        for line in grid:
+            count_x = 0
+            count_y = count_y + maze.cell_size
+            for cell in line:
+                maze_skin = pygame.Surface((maze.cell_size, maze.cell_size))
+                maze_skin.fill(RED)
+                if cell.cell_state == AVAIABLE:
+                    screen.blit(maze_skin, (count_x, count_y))
+                count_x = count_x + maze.cell_size
+        if time > 1000 / MAZE_SPEED:
+            time = 0
+            grid.pop(0)
+            grid.append(maze.get_line())
 
         # Update the display
         pygame.display.update()
