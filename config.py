65eaa9585ca52d6a4824966dc78db521c887bd7c
Gianluigi Dal Toso <gianluigi.toso@gmail.com>
2019-03-22 02:22:46 -0300

    Add the new colors to the config file

---
+++ config.py
@@ -0,0 +1,15 @@
+# Colors:
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (127, 255, 212)
+YELLOW = (255, 255, 0)
+ORANGE = (255, 140, 0)
+INDIGO = (0, 0, 128)
+VIOLET = (238, 130, 238)
+WHITE = (255, 255, 255)
+
+
+# Directions:
+STAY = 0
+RIGHT = 1
+LEFT = 2
