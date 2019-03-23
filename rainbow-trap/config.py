175b49fa87b820861bc0c46354748e15490dfdbe
lucasbeto <skybilobran@gmail.com>
2019-03-23 01:50:43 -0300

    Change application layout

---
+++ rainbow-trap/config.py
@@ -0,0 +1,32 @@
+# Screen Options
+SCREEN_WIDTH = 1000
+SCREEN_LENGTH = 1000
+
+# Colors
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (127, 255, 212)
+YELLOW = (255, 255, 0)
+ORANGE = (255, 140, 0)
+INDIGO = (0, 0, 128)
+VIOLET = (238, 130, 238)
+WHITE = (255, 255, 255)
+
+# Directions
+STAY = 0
+RIGHT = 1
+LEFT = 2
+
+# Cell States
+INVALID = 0
+WALL = 1
+AVAIABLE = 2
+
+# Game options
+INITIAL_X = 495
+INITIAL_Y = 50
+PLAYER_SIZE = 10
+MAZE_SPEED = 10
+
+FAT_X = 50
+FAT_Y = 50
